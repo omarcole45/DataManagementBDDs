12852004938a02ceefd383d2a6c825be0b56febb
omarcole45 <omirriuscoleman3028@yahoo.com>
2020-07-22 05:20:51 -0500

    adding new test for getUser resource

--- src/test/java/com/omardev/app/ws/restassuredtest/UsersWebServiceEndpointTest.java
+++ src/test/java/com/omardev/app/ws/restassuredtest/UsersWebServiceEndpointTest.java
@@ -4,19 +4,25 @@ import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.Alphanumeric;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+@TestMethodOrder(Alphanumeric.class)
 class UsersWebServiceEndpointTest {
 	
 	private final String CONTEXT_PATH= "/omar-app-ws";
 	private final String EMAIL_ADDRESS="air_man_j@yahoo.com";
 	private final String JSON = "application/json";
+	private static String authorization;
+	private static String userId;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -25,24 +31,51 @@ class UsersWebServiceEndpointTest {
 	}
 
 	@Test
-	void testUserLogin() {
+	void a() {
 		Map<String, Object> loginDetails = new HashMap<>();
 		loginDetails.put("email",EMAIL_ADDRESS);
 		loginDetails.put("password","test");
 		
 		Response response = given().contentType(JSON).
-		accept("application/json").body(loginDetails).when().post(CONTEXT_PATH + "/users/login").
+		accept(JSON).body(loginDetails).when().post(CONTEXT_PATH + "/users/login").
 		then().
 		statusCode(200).
 		contentType("").//For now i have content type returning an empty string
 		extract().
 		response();
 		
-		String authorization = response.header("Authorization");
-		String UserId= response.header("UserId");
+		 authorization = response.header("Authorization");
+		userId= response.header("UserId");
 		
 		assertNotNull(authorization);
-		assertNotNull(UserId);
+		assertNotNull(userId);
+		
+		
+	}
+	
+	@Test
+	void b() {
+		
+		Response response = given().header("Authorization", authorization)
+				.accept(JSON).when()
+				.get(CONTEXT_PATH + "/users/" + userId).then().statusCode(200).contentType(JSON).extract().response();
+		
+		String userPublicId = response.jsonPath().getString("userId");
+		String userEmail = response.jsonPath().getString("email");
+		String firstName = response.jsonPath().getString("firstName");
+		String lastName = response.jsonPath().getString("lastName");
+		List<Map<String, Object>> addresses = response.jsonPath().getList("addresses");
+		String addressId = addresses.get(0).get("addressId").toString();
+		
+		assertNotNull(userPublicId);
+		assertNotNull(userEmail);
+		assertNotNull(firstName);
+		assertNotNull(lastName);
+		assertEquals(EMAIL_ADDRESS, userEmail);
+		assertTrue(addresses.size() == 2);
+		assertTrue(addressId.length() == 30);
+		
+		
 		
 		
 	}
